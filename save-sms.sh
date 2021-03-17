aea293e276a30ef6d1eb5f4a341a279aa3d81958
Arthur Williams <taaparthur@gmail.com>
2021-03-17 02:49:21 -0700

    Cleanup

--- save-sms.sh
+++ save-sms.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/sh -e
 
 SMS_MESSAGE_DIR=${SMS_MESSAGE_DIR:-~/SMS}
 sms -d | {
     read -r _ date time tz
     timestamp="$date $time $tz"
     read -r _ number
+    echo "Timestamp $timestamp Number: $number"
+    [ -n "$number" ]
     msg="$(cat -)"
     echo "Saving $timestamp $number $msg"
-    SMS_DIR=$SMS_MESSAGE_DIR/$number/
+    SMS_DIR=$SMS_MESSAGE_DIR/$number
     if [ -n "$SMS_INDEX" ]; then
-        SMS_INDEX_DIR=$SMS_DIR/.index/
+        SMS_INDEX_DIR=$SMS_DIR/.index
         mkdir -p $SMS_INDEX_DIR
         SMS_INDEX_FILE=$SMS_DIR/.index/$SMS_INDEX
         if [ -f $SMS_INDEX_FILE ] && [ "$(cat $SMS_INDEX_FILE)" = "$timestamp" ]; then
