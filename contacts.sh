4d23bf3d708fe90fc3f9fb0df4ebfbc1997554f1
Arthur Williams <taaparthur@gmail.com>
2021-03-18 20:36:59 -0700

    Added script to parse contacts file

---
+++ contacts.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+CONTACTS_FILE=${CONTACTS_FILE:-$HOME/contacts.json}
+
+case "$1" in
+    list)
+        jq -r "keys[]" $CONTACTS_FILE
+        ;;
+    get-name)
+        jq -r "to_entries[]|select (.value.number | contains(\"$2\")) | .key" $CONTACTS_FILE
+        ;;
+    get-number)
+        number=$(jq -er ".$2.number" $CONTACTS_FILE || echo $2 | sed -E "s/[^0-9]+//g")
+        [ -z $number ]
+        echo $number
+esac
--- send-sms.sh
+++ send-sms.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -e
-CONTACTS_FILE=${CONTACTS_FILE:-$HOME/contacts.json}
-name=$1
-shift
-number=$(jq .$name.number $CONTACTS_FILE | sed -E "s/[^0-9]+//g")
-[ -z "$number" ]  && number=$name
+number=$(contacts get-number $1)
 echo "Sending message to $number"
 sms $number $@ | mqsend phone
