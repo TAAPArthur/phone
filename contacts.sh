a810dadc38089f39ec1df701c9e01f7defe3834a
Arthur Williams <taaparthur@gmail.com>
2021-03-20 23:41:14 -0700

    fixup! Updated contacts script

--- contacts.sh
+++ contacts.sh
@@ -6,10 +6,11 @@ case "$1" in
         jq -r "keys[]" $CONTACTS_FILE
         ;;
     get-name)
-        jq -r "to_entries[]|select (.value.number | contains(\"$2\")) | .key" $CONTACTS_FILE
+        number=$(echo $2| sed -E "s/[^0-9]+//g")
+        jq -er "to_entries[]|select (.value.number | gsub(\"[^0-9]\";\"\") | (select (. == \"$number\" or . == \"${number#1}\"))) | .key" $CONTACTS_FILE || echo $number
         ;;
     get-number)
-        number=$( (jq -er ".$2.number" $CONTACTS_FILE || echo $2) | sed -E "s/[^0-9]+//g")
+        number=$( (jq -er ".\"$2\".number" $CONTACTS_FILE || echo $2) | sed -E "s/[^0-9]+//g")
         [ -n "$number" ]
         echo $number
 esac
