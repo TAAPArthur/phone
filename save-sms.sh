c320c916eb258d9f20564cf017a4f5b438c49df4
Arthur Williams <taaparthur@gmail.com>
2021-03-27 16:43:00 -0700

    Delete received SMS messages

--- save-sms.sh
+++ save-sms.sh
@@ -15,11 +15,15 @@ sms -d | {
         mkdir -p $SMS_INDEX_DIR
         SMS_INDEX_FILE=$SMS_DIR/.index/$SMS_INDEX
         if [ -f $SMS_INDEX_FILE ] && [ "$(cat $SMS_INDEX_FILE)" = "$timestamp" ]; then
+            echo "AT+CMGD=$SMS_INDEX" | mqsend phone
             exit 0
         fi
     fi
     SMS_DATE_DIR="$SMS_DIR/$(date -I)"
     mkdir -p $SMS_DATE_DIR
     printf "%s\t%s\t%s\n\0" "$(date -Iseconds)" "$timestamp" "$msg" >> $SMS_DATE_DIR/sms.txt
-    [ -z "$SMS_INDEX" ] || printf "%s" $timestamp > $SMS_INDEX_FILE
+    if [ -n "$SMS_INDEX" ]; then
+        printf "%s" $timestamp > $SMS_INDEX_FILE
+        echo "AT+CMGD=$SMS_INDEX" | mqsend phone
+    fi
 }
