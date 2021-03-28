3236f4ecd6fa9e2176f525b2baf1973ce1327822
Arthur Williams <taaparthur@gmail.com>
2021-03-28 00:47:53 -0700

    fixup! Delete SMS after save

--- save-sms.sh
+++ save-sms.sh
@@ -7,21 +7,20 @@ sms -d "$1" | {
     read -r number
     echo "Timestamp $timestamp Number: $number"
     [ -n "$number" ]
-    SMS_DIR=$PHONE_DIR/$number/SMS
     msg="$(cat -)"
     echo "Saving $timestamp $number $msg"
     if [ -n "$SMS_INDEX" ]; then
         INDEX_DIR="$PHONE_DIR/.index"
         mkdir -p "$INDEX_DIR"
-        SMS_INDEX_FILE="$DIR/.index/$INDEX"
+        INDEX_FILE="$INDEX_DIR/$INDEX"
         if [ -f "$INDEX_FILE" ] && [ "$(cat "$INDEX_FILE")" = "$timestamp" ]; then
             exit 0
         fi
     fi
-    SMS_DATE_DIR="$SMS_DIR/$(date -I)"
+    SMS_DATE_DIR="$PHONE_DIR/$number/SMS/$(date -I)"
     mkdir -p "$SMS_DATE_DIR"
     printf "%s\t%s\t%s\n\0" "$(date -Iseconds)" "$timestamp" "$msg" >> "$SMS_DATE_DIR/sms.txt"
     if [ -n "$SMS_INDEX" ]; then
-        printf "%s" "$timestamp" > "$SMS_INDEX_FILE"
+        printf "%s" "$timestamp" > "$INDEX_FILE"
     fi
 }
