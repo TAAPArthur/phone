985f05c276d58a272d2704fac617540c9d84e1bd
Arthur Williams <taaparthur@gmail.com>
2021-03-28 00:40:17 -0700

    fixup! Hardening

--- save-sms.sh
+++ save-sms.sh
@@ -1,27 +1,27 @@
 #!/bin/sh -e
 
-sms -d $1 | {
+PHONE_DIR=${PHONE_DIR:-~/Phone}
+sms -d "$1" | {
     read -r date time tz
     timestamp="$date $time $tz"
     read -r number
     echo "Timestamp $timestamp Number: $number"
     [ -n "$number" ]
-    SMS_MESSAGE_DIR=${PHONE_DIR:-~/Phone}/$number/SMS
+    SMS_DIR=$PHONE_DIR/$number/SMS
     msg="$(cat -)"
     echo "Saving $timestamp $number $msg"
-    SMS_DIR=$SMS_MESSAGE_DIR/$number
     if [ -n "$SMS_INDEX" ]; then
-        SMS_INDEX_DIR=$SMS_DIR/.index
-        mkdir -p $SMS_INDEX_DIR
-        SMS_INDEX_FILE=$SMS_DIR/.index/$SMS_INDEX
-        if [ -f $SMS_INDEX_FILE ] && [ "$(cat $SMS_INDEX_FILE)" = "$timestamp" ]; then
+        INDEX_DIR="$PHONE_DIR/.index"
+        mkdir -p "$INDEX_DIR"
+        SMS_INDEX_FILE="$DIR/.index/$INDEX"
+        if [ -f "$INDEX_FILE" ] && [ "$(cat "$INDEX_FILE")" = "$timestamp" ]; then
             exit 0
         fi
     fi
     SMS_DATE_DIR="$SMS_DIR/$(date -I)"
-    mkdir -p $SMS_DATE_DIR
-    printf "%s\t%s\t%s\n\0" "$(date -Iseconds)" "$timestamp" "$msg" >> $SMS_DATE_DIR/sms.txt
+    mkdir -p "$SMS_DATE_DIR"
+    printf "%s\t%s\t%s\n\0" "$(date -Iseconds)" "$timestamp" "$msg" >> "$SMS_DATE_DIR/sms.txt"
     if [ -n "$SMS_INDEX" ]; then
-        printf "%s" $timestamp > $SMS_INDEX_FILE
+        printf "%s" "$timestamp" > "$SMS_INDEX_FILE"
     fi
 }
