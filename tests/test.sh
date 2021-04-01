55c18781e687b6fd5a002dbbc71aa92011d30488
Arthur Williams <taaparthur@gmail.com>
2021-03-31 17:12:46 -0700

    Test fixup

--- tests/test.sh
+++ tests/test.sh
@@ -2,30 +2,30 @@
 export CONTACTS_FILE=tests/sample_contacts.json
 failTest() {
     echo "############################"
-    . $1
+    . "$1"
     echo "############################"
     exit 3
 }
 for script in tests/0*.sh; do
-    printf "%s..." $script
+    printf "%s..." "$script"
     exec 3<<EOF
-$(grep '# ' $script | cut -d' ' -f2-)
+$(grep '# ' "$script" | cut -d' ' -f2-)
 EOF
     i=0
-    /bin/sh $script 1>/dev/null 2>&1
-    /bin/sh $script 2>/dev/null 3>/dev/null | {
+    /bin/sh "$script" 1>/dev/null 2>&1
+    /bin/sh "$script" 2>/dev/null 3>/dev/null | {
         while read -r output; do
             i=$((i+1))
             read -r expected <&3 || expected=""
             if [ "$output" != "$expected" ]; then
                 echo "failed on line $i"
-                printf "'%s'\nvs\n'%s';\n" "$output" "$expected"
-                failTest $script
+                printf "Ouptut vs expected\n'%s'\nvs\n'%s';\n" "$output" "$expected"
+                failTest "$script"
             fi
         done
         if [ -z "$expected" ]; then
             echo "failed on line $i"
-            failTest $script
+            failTest "$script"
         fi
         #read -r expected <&3 && exit 1
         echo "passed"
