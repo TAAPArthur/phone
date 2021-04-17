c13f3a63bbf2c23b8cbc76b996c554eac35116c3
Arthur Williams <taaparthur@gmail.com>
2021-04-17 13:49:47 -0700

    Termine messages with \n in ringd

--- ringd.sh
+++ ringd.sh
@@ -1,17 +1,18 @@
 #!/bin/sh -e
 
 if [ "$1" = "-s" ]; then
-    mqbus-send phone-ring "$2"
+    echo "$2" | mqsend phone-ring
+    exit
 fi
 
-mqbus-receive phone-ring |  {
+mqreceive phone-ring |  {
 
     RINGD_DIR=${XDG_CONFIG_DIR:-$HOME/.config}/ringd
     mkdir -p "$RINGD_DIR"
     export NUMBER TYPE SUBADDR SATYPE ALPHA VALIDITY
-    call --ring "$NUMBER"
     while IFS="," read -r NUMBER TYPE SUBADDR SATYPE ALPHA VALIDITY; do
         (
+            call --ring "$NUMBER"
             for file in "$RINGD_DIR"/*.sh; do
                 [ -r "$file" ] && . "$file"
             done
