e7e73ae04edbe3eff042e7cc5518885338a442ff
Arthur Williams <taaparthur@gmail.com>
2021-04-17 13:50:52 -0700

    Added tests fro spawnResponse

--- tests/ttyio_unit.c
+++ tests/ttyio_unit.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "../ttyio.h"
 
 char* processMetadata(char*buffer);
 extern char lastLabel[];
@@ -29,6 +30,14 @@ SCUTEST(test_receive_sms) {
     char buffer[16] ={0};
     int ret = read(fds[0], buffer, sizeof(buffer));
     assert(strcmp(buffer,"AT+CMGR=10\r\n")==0);
+}
+
+int spawnResponse(Response* response, const char* arg) ;
+SCUTEST(test_spawn_response) {
+    Response success = {"", .cmd="exit 0"};
+    Response failure = {"", .cmd="exit 1"};
+    assert(0 == spawnResponse(&success, ""));
+    assert(1 == spawnResponse(&failure, ""));
 }
 
 int main(int argc, char * argv[]) {
