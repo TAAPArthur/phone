7405d94af1eeaa3c022377273eacb437c4abe365
Arthur Williams <taaparthur@gmail.com>
2021-04-19 19:13:44 -0700

    [call] Added support for call relaed supplementary services

--- call.sh
+++ call.sh
@@ -21,7 +21,19 @@ case $action in
     -a)
         cmd="ATA" # answer call
         LOG_ACTION=ANSWER
-        ;;
+    ;;
+    -b|--drop-ball)
+        cmd="AT+CHLD=0" # terminate held calls
+        LOG_ACTION=CLEAN_HELD
+    ;;
+    -c|--connect)
+        cmd="AT+CHLD=4" # Connects active calls and drops off
+        LOG_ACTION=CONNECT
+    ;;
+    -f|--finish)
+        cmd="AT+CHLD=1$1" # terminate active call (all if not specified) and accept other call
+        LOG_ACTION=TERMINATE$1
+    ;;
     -h)
         if [ "$1" = "--all" ]; then
             cmd="AT+CHUP" # hang up all calls
@@ -29,12 +41,20 @@ case $action in
             cmd="ATH" # hang up call
         fi
         LOG_ACTION=HANGUP
-        ;;
-    -t)
+    ;;
+    -j|--join)
+        cmd="AT+CHLD=3" # Add held call to active call
+        LOG_ACTION=JOIN
+    ;;
+    -o|--hold)
+        cmd="AT+CHLD=2$1" # place active call (all but $1 if specified) on hold
+        LOG_ACTION=HOLD$1
+    ;;
+    -t|--dialtone)
         cmd="AT+VTS=$1" # dial tones
         LOG_ACTION=DIALTONE
         LOG_PARAM=$1
-        ;;
+    ;;
     -d|*)
         name=$1
         shift
